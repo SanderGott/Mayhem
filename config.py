244647c73f5b3227f8163111f862a9d67c8a408f
Sander <sander@gottschalksen.com>
2023-03-16 21:09:02 +0000

    a

---
+++ config.py
@@ -0,0 +1,12 @@
+# File containing constants used in the game
+
+WIDTH, HEIGHT = 1000, 1000
+FPS = 60
+BLACK = (0, 0, 0)
+
+ROTATE_AMOUNT = 4
+
+GRAVITY = 0.03
+SPEED = 0.1
+
+BULLET_SPEED = 7
---
+++ main.py
@@ -0,0 +1,12 @@
+
+from config import *
+from classes import *
+
+if __name__ == "__main__":
+
+    # Create the game
+    game = Mayhem()
+
+    # Run the game
+
+
